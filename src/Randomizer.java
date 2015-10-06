d1b6e3e0090c40ea74187cf6fa9e4f33394a1d25
priya121 <priya121@github.com>
2015-10-06 16:30:30 +0100

    Inlined return statement.

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -4,7 +4,6 @@ public class Randomizer {
 
     public int numberGenerator(int max) {
         Random randomGenerator  = new Random();
-        int value = randomGenerator.nextInt(max);
-        return value;
+        return randomGenerator.nextInt(max);
     }
 }
