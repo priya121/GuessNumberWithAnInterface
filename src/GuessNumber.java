005dbab27110a533d7d1413d2ef985d08e02391a
priya121 <priya121@github.com>
2015-10-13 11:33:33 +0100

    Console Interface added.

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,17 +1,25 @@
 public class GuessNumber {
 
-    private IoInterface io;
+    private static IoInterface io;
     private int myNumber;
     private boolean isOver;
     String userPrompt;
+    String greeting;
     private int tries;
 
     public GuessNumber(IoInterface io, int myNumber) {
         this.io = io;
-        this.myNumber = 2;
+        this.myNumber = myNumber;
         this.isOver = false;
         this.userPrompt = "";
         this.tries = 0;
+    }
+
+    public static void main(String args[]) {
+        IoInterface io = new ConsoleIO(System.in, System.out);
+        GuessNumber newGame = new GuessNumber(io, new Randomizer().numberGenerator(101));
+        newGame.initialGreeting();
+        newGame.gameLoop(io.getInput());
     }
 
     public int getUserInput() {
@@ -50,5 +58,10 @@ public class GuessNumber {
     public String showUserCorrectGuess(int guess) {
         userPrompt = io.showUserCorrectGuess(guess);
         return userPrompt;
+    }
+
+    public String initialGreeting() {
+        greeting = io.initialPrompt();
+        return greeting;
     }
 }
